a699e41fdf59c2b11f3ca7ca613a762b733ca6a2
Victor Puchkarev <puchkarev@google.com>
2024-05-16 16:44:26 -0700

    splitting up plotting from simulation

---
+++ plots.py
@@ -0,0 +1,161 @@
+# Generates Plots
+import math
+import numpy
+
+from slam_geometry import normalize_angle
+
+class PlotData:
+  def __init__(self):
+    self.vals_frame_nums = []
+    self.vals_pose_error = []
+    self.vals_angle_error = []
+    self.vals_pose_std_dev = []
+    self.vals_angle_std_dev = []
+    self.vals_particles = []
+    self.vals_matched_segments = []
+    self.vals_total_segments = []
+    self.vals_matched_corners = []
+    self.vals_total_corners = []
+    self.vals_localized = []
+    self.vals_actual_pose_x = []
+    self.vals_actual_pose_y = []
+    self.vals_estimated_pose_x = []
+    self.vals_estimated_pose_y = []
+
+  def collect_data(self, mapping, robot):
+    if len(self.vals_frame_nums) == 0:
+      self.vals_frame_nums.append(0)
+    else:
+      self.vals_frame_nums.append(max(self.vals_frame_nums) + 1)
+
+    if robot and mapping:
+      self.vals_pose_error.append(math.hypot(robot.position[0] - mapping.robot_mean[0], \
+                                             robot.position[1] - mapping.robot_mean[1]))
+      self.vals_angle_error.append(numpy.rad2deg(normalize_angle(robot.position[2] - mapping.robot_mean[2])))
+
+    if mapping:
+      self.vals_pose_std_dev.append(math.sqrt(mapping.robot_covariance[0][0] + mapping.robot_covariance[1][1]))
+      self.vals_angle_std_dev.append(numpy.rad2deg(math.sqrt(mapping.robot_covariance[2][2])))
+      self.vals_particles.append(len(mapping.poses))
+      self.vals_matched_segments.append(len(mapping.segment_associations))
+      self.vals_total_segments.append(len(mapping.segment_associations) + len(mapping.new_segments))
+      self.vals_matched_corners.append(len(mapping.corner_associations))
+      self.vals_total_corners.append(len(mapping.corner_associations) + len(mapping.new_corners))
+      self.vals_estimated_pose_x.append(mapping.robot_mean[0])
+      self.vals_estimated_pose_y.append(mapping.robot_mean[1])
+      if mapping.localized:
+        self.vals_localized.append(100)
+      else:
+        self.vals_localized.append(0)
+
+    if robot:
+      self.vals_actual_pose_x.append(robot.position[0])
+      self.vals_actual_pose_y.append(robot.position[1])
+
+  def truncate(self, want_elements):
+    def tr(list):
+      if len(list) > want_elements:
+        del list[:-want_elements]
+
+    tr(self.vals_frame_nums)
+    tr(self.vals_pose_error)
+    tr(self.vals_angle_error)
+    tr(self.vals_pose_std_dev)
+    tr(self.vals_angle_std_dev)
+    tr(self.vals_particles)
+    tr(self.vals_matched_segments)
+    tr(self.vals_total_segments)
+    tr(self.vals_matched_corners)
+    tr(self.vals_total_corners)
+    tr(self.vals_localized)
+    tr(self.vals_actual_pose_x)
+    tr(self.vals_actual_pose_y)
+    tr(self.vals_estimated_pose_x)
+    tr(self.vals_estimated_pose_y)
+
+  def plot_reality(self, robot, map_plot):
+    # show the actual map that the robot understands
+    for segment in robot.segments:
+      map_plot.plot([segment[0][0], segment[1][0]], \
+                    [segment[0][1], segment[1][1]], 'k-')
+
+    # show the real robot position
+    map_plot.plot([robot.position[0]], \
+                  [robot.position[1]], 'bx', markersize=10)
+
+  def plot_mapping(self, mapping, map_plot):
+    # show the robot position distribution
+    map_plot.plot([p[0] for p in mapping.poses], \
+                  [p[1] for p in mapping.poses], 'ro', markersize=1)
+
+    # show the mean of the distribution
+    map_plot.plot([p[0] for p in mapping.poses], \
+                  [p[1] for p in mapping.poses], 'rx', markersize=5)
+
+    # show the lidar returns
+    map_plot.plot([p[0] for p in mapping.cartesian_points], \
+                  [p[1] for p in mapping.cartesian_points], 'bo', markersize=1)
+
+    # show the features that we have from mapping
+    for segment_association in mapping.segment_associations:
+      segment = segment_association[0]
+      map_plot.plot([segment[0][0], segment[1][0]], \
+                    [segment[0][1], segment[1][1]], 'g-')
+    for segment in mapping.new_segments:
+      map_plot.plot([segment[0][0], segment[1][0]], \
+                    [segment[0][1], segment[1][1]], 'r-')
+    for corner_association in mapping.corner_associations:
+      corner = corner_association[0]
+      map_plot.plot([corner[0]], \
+                    [corner[1]], 'go', markersize = 5)
+    for corner in mapping.new_corners:
+      map_plot.plot([corner[0]], \
+                    [corner[1]], 'ro', markersize = 5)
+
+  def plot_graphs(self, map_plot, graph_plot1, graph_plot2, graph_plot3, graph_plot4):
+    if len(self.vals_actual_pose_x) == len(self.vals_actual_pose_y):
+      map_plot.plot(self.vals_actual_pose_x, self.vals_actual_pose_y, 'k-')
+    if len(self.vals_estimated_pose_x) == len(self.vals_estimated_pose_y):
+      map_plot.plot(self.vals_estimated_pose_x, self.vals_estimated_pose_y, 'r-')
+
+    legend1 = []
+    if len(self.vals_frame_nums) == len(self.vals_pose_error):
+      graph_plot1.plot(self.vals_frame_nums, self.vals_pose_error, 'k-')
+      legend1.append("pose_error")
+    if len(self.vals_frame_nums) == len(self.vals_pose_std_dev):
+      graph_plot1.plot(self.vals_frame_nums, self.vals_pose_std_dev, 'r-')
+      legend1.append("estimated_error")
+    graph_plot1.legend(legend1)
+
+    legend2 = []
+    if len(self.vals_frame_nums) == len(self.vals_angle_error):
+      graph_plot2.plot(self.vals_frame_nums, self.vals_angle_error, 'k-')
+      legend2.append("angle_error")
+    if len(self.vals_frame_nums) == len(self.vals_angle_std_dev):
+      graph_plot2.plot(self.vals_frame_nums, self.vals_angle_std_dev, 'r-')
+      legend2.append("estimated_error")
+    graph_plot2.legend(legend2)
+
+    legend3 = []
+    if len(self.vals_frame_nums) == len(self.vals_matched_segments):
+      graph_plot3.plot(self.vals_frame_nums, self.vals_matched_segments)
+      legend3.append("matched_segments")
+    if len(self.vals_frame_nums) == len(self.vals_total_segments):
+      graph_plot3.plot(self.vals_frame_nums, self.vals_total_segments)
+      legend3.append("total_segments")
+    if len(self.vals_frame_nums) == len(self.vals_matched_corners):
+      graph_plot3.plot(self.vals_frame_nums, self.vals_matched_corners)
+      legend3.append("matched_corners")
+    if len(self.vals_frame_nums) == len(self.vals_total_corners):
+      graph_plot3.plot(self.vals_frame_nums, self.vals_total_corners)
+      legend3.append("total_corners")
+    graph_plot3.legend(legend3)
+
+    legend4 = []
+    if len(self.vals_frame_nums) == len(self.vals_localized):
+      graph_plot4.plot(self.vals_frame_nums, self.vals_localized)
+      legend4.append("localized")
+    if len(self.vals_frame_nums) == len(self.vals_particles):
+      graph_plot4.plot(self.vals_frame_nums, self.vals_particles)
+      legend4.append("particles")
+    graph_plot4.legend(legend4)
--- simulate.py
+++ simulate.py
@@ -6,52 +6,8 @@ import numpy
 
 from slam_geometry import normalize_angle
 from slam import MappingEnvironment
-from simulated_robot import SimulatedRobot
-
-class PlotData:
-  def __init__(self):
-    self.vals_frame_nums = []
-    self.vals_pose_error = []
-    self.vals_angle_error = []
-    self.vals_pose_std_dev = []
-    self.vals_angle_std_dev = []
-    self.vals_particles = []
-    self.vals_matched_segments = []
-    self.vals_total_segments = []
-    self.vals_matched_corners = []
-    self.vals_total_corners = []
-    self.vals_localized = []
-
-    self.vals_actual_pose_x = []
-    self.vals_actual_pose_y = []
-    self.vals_estimated_pose_x = []
-    self.vals_estimated_pose_y = []
-
-  def grow_list(self):
-    if len(self.vals_frame_nums) == 0:
-      self.vals_frame_nums.append(0)
-    else:
-      self.vals_frame_nums.append(max(self.vals_frame_nums) + 1)
-
-  def truncate(self):
-    want_elements = 100
-    if len(self.vals_frame_nums) > want_elements:
-      self.vals_frame_nums = self.vals_frame_nums[-want_elements:]
-      self.vals_pose_error = self.vals_pose_error[-want_elements:]
-      self.vals_angle_error = self.vals_angle_error[-want_elements:]
-      self.vals_pose_std_dev = self.vals_pose_std_dev[-want_elements:]
-      self.vals_angle_std_dev = self.vals_angle_std_dev[-want_elements:]
-      self.vals_particles = self.vals_particles[-want_elements:]
-      self.vals_matched_segments = self.vals_matched_segments[-want_elements:]
-      self.vals_total_segments = self.vals_total_segments[-want_elements:]
-      self.vals_matched_corners = self.vals_matched_corners[-want_elements:]
-      self.vals_total_corners = self.vals_total_corners[-want_elements:]
-      self.vals_localized = self.vals_localized[-want_elements:]
-
-      self.vals_actual_pose_x = self.vals_actual_pose_x[-want_elements:]
-      self.vals_actual_pose_y = self.vals_actual_pose_y[-want_elements:]
-      self.vals_estimated_pose_x = self.vals_estimated_pose_x[-want_elements:]
-      self.vals_estimated_pose_y = self.vals_estimated_pose_y[-want_elements:]
+from robot import SimulatedRobot
+from plots import PlotData
 
 if __name__ == '__main__':
   # Define the environment
@@ -118,6 +74,7 @@ if __name__ == '__main__':
       move_distance = 0
       turn_angle = 0
 
+    # if we want to play around with number of sample points
     if use_localize and scale_points:
       if mapping.localized and len(mapping.poses) > 10:
         mapping.reinitialize_particles(len(mapping.poses) - 1)
@@ -136,107 +93,22 @@ if __name__ == '__main__':
     if use_localize:
       mapping.lidar_update(robot.sense_environment())
 
-  def plot_map_plot():
-    # Re-initialize the plot
-    map_plot.cla()
-    map_plot.set_aspect('equal')
-    map_plot.set_xlim(0, 600)
-    map_plot.set_ylim(0, 600)
-
-    # show the actual robot position
-    if show_real_position:
-      map_plot.plot([robot.position[0]], [robot.position[1]], 'bo', markersize=10)
-
-    # show the real segments from the environment.
-    if show_real_map:
-      for segment in robot.segments:
-        map_plot.plot([segment[0][0], segment[1][0]], [segment[0][1], segment[1][1]], 'k-')
-
-    # show the robot positions
-    if show_mapped_position_all:
-      map_plot.plot([p[0] for p in mapping.poses], [p[1] for p in mapping.poses], 'ro', markersize=1)
-
-    # show the mean of the robot position
-    if show_mapped_position_mean:
-      map_plot.plot([mapping.robot_mean[0]], [mapping.robot_mean[1]], 'ro', markersize=4)
-
-    # show the lidar returns as understood by the mapping
-    if show_mapped_lidar and len(mapping.cartesian_points) != 0:
-      map_plot.plot([p[0] for p in mapping.cartesian_points], [p[1] for p in mapping.cartesian_points], 'bo')
-
-    # show the map as understood by the mapping
-    if show_mapped_map:
-      for segment_association in mapping.segment_associations:
-        segment = segment_association[0]
-        map_plot.plot([segment[0][0], segment[1][0]], [segment[0][1], segment[1][1]], 'g-')
-
-      for segment in mapping.new_segments:
-        map_plot.plot([segment[0][0], segment[1][0]], [segment[0][1], segment[1][1]], 'r-')
-      for corner_association in mapping.corner_associations:
-        corner = corner_association[0]
-        map_plot.plot([corner[0]], [corner[1]], 'go', markersize = 5)
-      for corner in mapping.new_corners:
-        map_plot.plot([corner[0]], [corner[1]], 'ro', markersize = 5)
-
-  def plot_graphs():
-    # Compute the values over time.
-    plot_data.grow_list()
-    plot_data.vals_pose_error.append(math.hypot(robot.position[0] - mapping.robot_mean[0], \
-                                                robot.position[1] - mapping.robot_mean[1]))
-    plot_data.vals_angle_error.append(numpy.rad2deg(normalize_angle(robot.position[2] - mapping.robot_mean[2])))
-    plot_data.vals_pose_std_dev.append(math.sqrt(mapping.robot_covariance[0][0] + mapping.robot_covariance[1][1]))
-    plot_data.vals_angle_std_dev.append(numpy.rad2deg(math.sqrt(mapping.robot_covariance[2][2])))
-    plot_data.vals_particles.append(len(mapping.poses))
-    plot_data.vals_matched_segments.append(len(mapping.segment_associations))
-    plot_data.vals_total_segments.append(len(mapping.segment_associations) + len(mapping.new_segments))
-    plot_data.vals_matched_corners.append(len(mapping.corner_associations))
-    plot_data.vals_total_corners.append(len(mapping.corner_associations) + len(mapping.new_corners))
-
-    if mapping.localized:
-      plot_data.vals_localized.append(1)
-    else:
-      plot_data.vals_localized.append(0)
-
-    plot_data.vals_actual_pose_x.append(robot.position[0])
-    plot_data.vals_actual_pose_y.append(robot.position[1])
-    plot_data.vals_estimated_pose_x.append(mapping.robot_mean[0])
-    plot_data.vals_estimated_pose_y.append(mapping.robot_mean[1])
-
-    plot_data.truncate()
-
-    # plot the past paths on the map
-    if show_past_path:
-      map_plot.plot(plot_data.vals_actual_pose_x, plot_data.vals_actual_pose_y, 'r-')
-      map_plot.plot(plot_data.vals_estimated_pose_x, plot_data.vals_estimated_pose_y, 'k-')
-
-    # plot the results
-    graph_plot1.cla()
-    graph_plot1.plot(plot_data.vals_frame_nums, plot_data.vals_pose_error, 'k-',)
-    graph_plot1.plot(plot_data.vals_frame_nums, plot_data.vals_pose_std_dev, 'r-')
-    graph_plot1.legend(["pose_error", "pose_std_dev"])
-
-    graph_plot2.cla()
-    graph_plot2.plot(plot_data.vals_frame_nums, plot_data.vals_angle_error, 'k-')
-    graph_plot2.plot(plot_data.vals_frame_nums, plot_data.vals_angle_std_dev, 'r-')
-    graph_plot2.legend(["angle_error", "angle_std_dev"])
-
-    graph_plot3.cla()
-    graph_plot3.plot(plot_data.vals_frame_nums, plot_data.vals_localized, 'k-')
-    graph_plot3.plot(plot_data.vals_frame_nums, plot_data.vals_matched_segments)
-    graph_plot3.plot(plot_data.vals_frame_nums, plot_data.vals_total_segments)
-    graph_plot3.plot(plot_data.vals_frame_nums, plot_data.vals_matched_corners)
-    graph_plot3.plot(plot_data.vals_frame_nums, plot_data.vals_total_corners)
-    graph_plot3.legend(["localized", "matched_segments", "total_segments", "matched_corners", "total_corners"])
-
-    graph_plot4.cla()
-    graph_plot4.plot(plot_data.vals_frame_nums, plot_data.vals_particles, 'k-')
-    graph_plot4.legend(["particles", "matched_segments", "total_segments", "matched_corners", "total_corners"])
-
-
   def update(frame):
     handle_robot()
-    plot_map_plot()
-    plot_graphs()
+
+    plot_data.collect_data(mapping = mapping, robot = robot)
+    plot_data.truncate(100)
+
+    map_plot.cla()
+    map_plot.set_aspect('equal')
+    plot_data.plot_reality(robot = robot, map_plot = map_plot)
+    plot_data.plot_mapping(mapping = mapping, map_plot = map_plot)
+
+    graph_plot1.cla()
+    graph_plot2.cla()
+    graph_plot3.cla()
+    graph_plot4.cla()
+    plot_data.plot_graphs(map_plot, graph_plot1, graph_plot2, graph_plot3, graph_plot4)
 
   ani = animation.FuncAnimation(fig1, update, frames=100, repeat=True, cache_frame_data=False, interval=100)
   plt.show()
